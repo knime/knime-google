0fec778041c19bbe466e9ebc5cf55ae69ca174c3
Ole Ostergaard <ole.ostergaard@knime.com>
2018-10-25 15:59:32 +0200

    AP-10435: Add general (OAuth2) Google Authentication to GoogleApiConnection. Add helper classes for authentication.
    AP-10435 (Generalize Google Authentication)
    https://knime-com.atlassian.net/browse/AP-10435

--- org.knime.google.api/src/org/knime/google/api/data/GoogleApiConnection.java
+++ org.knime.google.api/src/org/knime/google/api/data/GoogleApiConnection.java
@@ -52,13 +52,19 @@ import java.io.File;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
+import java.util.List;
 
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.ModelContentRO;
 import org.knime.core.node.ModelContentWO;
+import org.knime.google.api.nodes.authconnector.auth.GoogleAuthLocationType;
+import org.knime.google.api.nodes.authconnector.auth.GoogleAuthentication;
+import org.knime.google.api.nodes.authconnector.util.KnimeGoogleAuthScope;
+import org.knime.google.api.nodes.authconnector.util.KnimeGoogleAuthScopeRegistry;
 
+import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
@@ -68,9 +74,11 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 /**
  * Object that represents a connection to the Google API.
  *
- * Use the {@link GoogleCredential} object returned by {@link #getCredential()} to build objects for a specific Google API.
+ * Use the {@link GoogleCredential} object returned by {@link #getCredential()} to build objects for a specific Google
+ * API.
  *
  * @author Patrick Winter, KNIME AG, Zurich, Switzerland
+ * @author Ole Ostergaard, KNIME GmbH, Konstanz, Germany
  */
 public final class GoogleApiConnection {
 
@@ -84,13 +92,29 @@ public final class GoogleApiConnection {
 
     private static final String CFG_SCOPES = "scopes";
 
-    private GoogleCredential m_credential;
+    private static final String CFG_CREDENTIAL_LOCATION_TYPE = "credentialLocationType";
+
+    private static final String CFG_CREDENTIAL_LOCATION = "credentialLocation";
+
+    private static final String CFG_STORED_CREDENTIAL = "storedCredential";
+
+    private static final String CFG_KNIME_SCOPES = "knimeScopes";
+
+    private Credential m_credential;
 
     private String m_serviceAccountEmail;
 
     private String m_keyFileLocation;
 
+    private GoogleAuthLocationType m_credentialLocationType;
+
+    private String m_credentialLocation;
+
+    private String m_storedCredential;
+
     private String[] m_scopes;
+
+    private List<KnimeGoogleAuthScope> m_knimeGoogleScopes;
 
     /**
      * Creates a new {@link GoogleApiConnection}.
@@ -102,14 +126,72 @@ public final class GoogleApiConnection {
      * @throws IOException If the key file was not accessible
      */
     public GoogleApiConnection(final String serviceAccountEmail, final String keyFileLocation, final String... scopes)
-            throws GeneralSecurityException, IOException {
+        throws GeneralSecurityException, IOException {
         m_serviceAccountEmail = serviceAccountEmail;
         m_keyFileLocation = keyFileLocation;
         m_scopes = scopes;
-        m_credential =
-                new GoogleCredential.Builder().setTransport(HTTP_TRANSPORT).setJsonFactory(JSON_FACTORY)
-                        .setServiceAccountId(serviceAccountEmail).setServiceAccountScopes(Arrays.asList(scopes))
-                        .setServiceAccountPrivateKeyFromP12File(new File(keyFileLocation)).build();
+        m_credential = new GoogleCredential.Builder().setTransport(HTTP_TRANSPORT).setJsonFactory(JSON_FACTORY)
+            .setServiceAccountId(serviceAccountEmail).setServiceAccountScopes(Arrays.asList(scopes))
+            .setServiceAccountPrivateKeyFromP12File(new File(keyFileLocation)).build();
+        m_credentialLocationType = null;
+    }
+
+    /**
+     * Creates a new {@link GoogleApiConnection}
+     *
+     * @param locationType The credential location type
+     * @param credentialLocation
+     * @param knimeGoogleScopes The scopes for the credentials.
+     * @throws IOException If the credentials cannot be read from the credentiallocationType
+     */
+    public GoogleApiConnection(final GoogleAuthLocationType locationType, final String credentialLocation,
+        final List<KnimeGoogleAuthScope> knimeGoogleScopes) throws IOException {
+        m_scopes = KnimeGoogleAuthScopeRegistry.getAuthScopes(knimeGoogleScopes)
+            .toArray(new String[knimeGoogleScopes.size()]);
+        m_credentialLocationType = locationType;
+        m_credentialLocation = credentialLocation;
+        if (m_credentialLocationType.equals(GoogleAuthLocationType.NODE)) {
+            m_storedCredential = GoogleAuthentication.getByteStringFromFile(credentialLocation);
+        }
+        m_knimeGoogleScopes = knimeGoogleScopes;
+        m_credential = GoogleAuthentication.getNoAuthCredential(m_credentialLocationType, m_credentialLocation,
+            m_knimeGoogleScopes);
+
+    }
+
+    /**
+     * Private constructor used to load connection.
+     *
+     * @param locationType The credential location type
+     * @param serviceAccountEmail The service account email if appropriate
+     * @param keyFileLocation The key file location if appropriate
+     * @param scopes The scopes used for the credentials
+     * @throws GeneralSecurityException If there was a problem with the key file
+     * @throws IOException If the key file or the credential location is not accessible
+     */
+    private GoogleApiConnection(final GoogleAuthLocationType locationType, final String credentialLocation,
+        final String storedCredential, final String serviceAccountEmail, final String keyFileLocation,
+        final List<KnimeGoogleAuthScope> knimeAuthScopes, final String... scopes)
+        throws GeneralSecurityException, IOException {
+        m_serviceAccountEmail = serviceAccountEmail;
+        m_keyFileLocation = keyFileLocation;
+        m_scopes = scopes;
+        m_credentialLocationType = locationType;
+        m_credentialLocation = credentialLocation;
+        m_storedCredential = storedCredential;
+        m_knimeGoogleScopes = knimeAuthScopes;
+        if (locationType == null) {
+            m_credential = new GoogleCredential.Builder().setTransport(HTTP_TRANSPORT).setJsonFactory(JSON_FACTORY)
+                .setServiceAccountId(serviceAccountEmail).setServiceAccountScopes(Arrays.asList(scopes))
+                .setServiceAccountPrivateKeyFromP12File(new File(keyFileLocation)).build();
+        } else {
+            if (m_credentialLocationType.equals(GoogleAuthLocationType.NODE)
+                && !(new File(m_credentialLocation).exists())) {
+                m_credentialLocation = GoogleAuthentication.getTempCredentialPath(m_storedCredential);
+            }
+            m_credential = GoogleAuthentication.getNoAuthCredential(m_credentialLocationType, m_credentialLocation,
+                m_knimeGoogleScopes);
+        }
     }
 
     /**
@@ -120,30 +202,37 @@ public final class GoogleApiConnection {
      * @throws IOException If the key file was not accessible
      * @throws InvalidSettingsException If the model was invalid
      */
-    public GoogleApiConnection(final ModelContentRO model) throws GeneralSecurityException, IOException,
-            InvalidSettingsException {
-        this(model.getString(CFG_SERVICE_ACCOUNT_EMAIL), model.getString(CFG_KEY_FILE_LOCATION), model
-                .getStringArray(CFG_SCOPES));
+    public GoogleApiConnection(final ModelContentRO model)
+        throws GeneralSecurityException, IOException, InvalidSettingsException {
+        this(
+            model.containsKey(CFG_CREDENTIAL_LOCATION_TYPE)
+                ? GoogleAuthLocationType.valueOf(model.getString(CFG_CREDENTIAL_LOCATION_TYPE)) : null,
+            model.getString(CFG_CREDENTIAL_LOCATION), model.getString(CFG_STORED_CREDENTIAL),
+            model.getString(CFG_SERVICE_ACCOUNT_EMAIL), model.getString(CFG_KEY_FILE_LOCATION),
+            model.containsKey(CFG_KNIME_SCOPES) ?
+            KnimeGoogleAuthScopeRegistry.getInstance().getScopesFromString(
+                Arrays.asList(model.getStringArray(CFG_KNIME_SCOPES))) : null,
+            model.getStringArray(CFG_SCOPES));
     }
 
     /**
      * @return The {@link HttpTransport} instance to be used to build Google API objects
      */
-    public HttpTransport getHttpTransport() {
+    public static HttpTransport getHttpTransport() {
         return HTTP_TRANSPORT;
     }
 
     /**
      * @return The {@link JsonFactory} instance to be used to build Google API objects
      */
-    public JsonFactory getJsonFactory() {
+    public static JsonFactory getJsonFactory() {
         return JSON_FACTORY;
     }
 
     /**
      * @return The {@link GoogleCredential} object used to build specific Google API objects
      */
-    public GoogleCredential getCredential() {
+    public Credential getCredential() {
         return m_credential;
     }
 
@@ -154,6 +243,15 @@ public final class GoogleApiConnection {
         model.addString(CFG_SERVICE_ACCOUNT_EMAIL, m_serviceAccountEmail);
         model.addString(CFG_KEY_FILE_LOCATION, m_keyFileLocation);
         model.addStringArray(CFG_SCOPES, m_scopes);
+        if (m_credentialLocationType != null) {
+            model.addString(CFG_CREDENTIAL_LOCATION_TYPE, m_credentialLocationType.getActionCommand());
+        }
+        model.addString(CFG_CREDENTIAL_LOCATION, m_credentialLocation);
+        model.addString(CFG_STORED_CREDENTIAL, m_storedCredential);
+        if (m_knimeGoogleScopes != null) {
+            model.addStringArray(CFG_KNIME_SCOPES,
+                KnimeGoogleAuthScopeRegistry.getKnimeGoogleScopeIds(m_knimeGoogleScopes));
+        }
     }
 
     /**
@@ -172,6 +270,10 @@ public final class GoogleApiConnection {
         eb.append(m_serviceAccountEmail, con.m_serviceAccountEmail);
         eb.append(m_keyFileLocation, con.m_keyFileLocation);
         eb.append(m_scopes, con.m_scopes);
+        eb.append(m_credentialLocationType, con.m_credentialLocationType);
+        eb.append(m_credentialLocation, con.m_credentialLocation);
+        eb.append(m_storedCredential, con.m_storedCredential);
+        eb.append(m_knimeGoogleScopes, con.m_knimeGoogleScopes);
         return eb.isEquals();
     }
 
@@ -184,6 +286,10 @@ public final class GoogleApiConnection {
         hcb.append(m_serviceAccountEmail);
         hcb.append(m_keyFileLocation);
         hcb.append(m_scopes);
+        hcb.append(m_credentialLocationType.getActionCommand());
+        hcb.append(m_credentialLocation);
+        hcb.append(m_storedCredential);
+        hcb.append(m_knimeGoogleScopes);
         return hcb.hashCode();
     }
 
@@ -193,8 +299,23 @@ public final class GoogleApiConnection {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("Service account email address:\n" + m_serviceAccountEmail + "\n\n");
-        sb.append("Key file:\n" + m_keyFileLocation + "\n\n");
+        if (m_credentialLocationType == null) {
+            sb.append("Service account email address:\n" + m_serviceAccountEmail + "\n\n");
+            sb.append("Key file:\n" + m_keyFileLocation + "\n\n");
+        } else {
+            sb.append("Google OAuth 2.0 Authentication \n\n");
+            sb.append("Authentication Keys: " + m_credentialLocationType.getText() + "\n\n");
+            switch (m_credentialLocationType) {
+                case MEMORY:
+                    //
+                    break;
+                case NODE:
+                    break;
+                default:
+                    sb.append("Location: " + m_credentialLocation + "\n\n");
+            }
+
+        }
         sb.append("Scopes:\n");
         for (String scope : m_scopes) {
             sb.append(scope + "\n");
---
+++ org.knime.google.api/src/org/knime/google/api/nodes/authconnector/auth/GoogleAuthentication.java
@@ -0,0 +1,233 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Oct 8, 2018 (oole): created
+ */
+package org.knime.google.api.nodes.authconnector.auth;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.util.Base64;
+import java.util.List;
+
+import org.knime.core.util.FileUtil;
+import org.knime.google.api.analytics.nodes.connector.GoogleAnalyticsConnectorFactory;
+import org.knime.google.api.nodes.authconnector.util.KnimeGoogleAuthScope;
+import org.knime.google.api.nodes.authconnector.util.KnimeGoogleAuthScopeRegistry;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.auth.oauth2.StoredCredential;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.DataStoreFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.client.util.store.MemoryDataStoreFactory;
+
+/**
+ * Class that handles the authentication with Google Services.
+ *
+ * @author Ole Ostergaard, KNIME GmbH, Konstanz, Germany
+ */
+public class GoogleAuthentication {
+
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY = new JacksonFactory();
+
+    /** Global instance of the HTTP transport. */
+    private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+
+    /** The name of the client secret JSON file **/
+    private static final String CLIENT_SECRET = "client_secret_google_sheets.json";
+
+    private static final String DEFAULT_KNIME_USER = "knimeGoogleUser";
+
+    /**
+     * If the user is already authenticated the corresponding credentials will be return, otherwise an authorization
+     * pop-up will open which asks the user to authenticate with the provided scopes.
+     *
+     * @param type The {@link GoogleAuthLocationType} used to store the credentials
+     * @param credentialPath The path to the credentials
+     * @param scopes The scopes which should be asked for authorization
+     * @return The authenticated credentials
+     * @throws IOException
+     */
+    public static Credential getCredential(final GoogleAuthLocationType type, final String credentialPath,
+        final List<KnimeGoogleAuthScope> scopes) throws IOException {
+        DataStoreFactory credentialDataStoreFactory = getCredentialDataStoreFactory(type, credentialPath);
+        final GoogleAuthorizationCodeFlow flow = getAuthorizationCodeFlow(credentialDataStoreFactory, scopes);
+        Credential credential = flow.loadCredential(DEFAULT_KNIME_USER);
+        if (credential == null) {
+            credential =
+                new CustomAuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize(DEFAULT_KNIME_USER);
+        }
+        return credential;
+    }
+
+    /**
+     * Returns credentials for an already authenticated user, or NULL if there is no authentication present.
+     *
+     * @param type The {@link GoogleAuthLocationType}
+     * @param credentialPath The credentials path
+     * @param scopes The scopes that should be used with the credentials
+     * @return authorized credentials if they are available
+     * @throws IOException
+     */
+    public static Credential getNoAuthCredential(final GoogleAuthLocationType type, final String credentialPath,
+        final List<KnimeGoogleAuthScope> scopes) throws IOException {
+        DataStoreFactory credentialDataStoreFactory = getCredentialDataStoreFactory(type, credentialPath);
+        final GoogleAuthorizationCodeFlow flow = getAuthorizationCodeFlow(credentialDataStoreFactory, scopes);
+        Credential credential = flow.loadCredential(DEFAULT_KNIME_USER);
+        return credential;
+    }
+
+    /**
+     * Returns the {@link DataStoreFactory} corresponding to the given {@link GoogleAuthLocationType}.
+     *
+     * @param locationType The {@link GoogleAuthLocationType} for which the {@link DataStoreFactory} should be returned
+     * @param credentialPath The credential path for appropriate credential location types
+     * @return The {@link DataStoreFactory} corresponding to the given {@link GoogleAuthLocationType} and path
+     * @throws IOException If the {@link DataStoreFactory} cannot be created
+     */
+    private static DataStoreFactory getCredentialDataStoreFactory(final GoogleAuthLocationType locationType,
+        final String credentialPath) throws IOException {
+        DataStoreFactory credentialDataStoreFactory = null;
+        switch (locationType) {
+            case MEMORY:
+                credentialDataStoreFactory = MemoryDataStoreFactory.getDefaultInstance();
+                break;
+            case NODE:
+                credentialDataStoreFactory = new FileDataStoreFactory(new File(credentialPath));
+                break;
+            case FILESYSTEM:
+                credentialDataStoreFactory = new FileDataStoreFactory(new File(credentialPath));
+                break;
+        }
+        return credentialDataStoreFactory;
+    }
+
+    /**
+     * Builds the {@link GoogleAuthorizationCodeFlow}, which is responsible for open the authorization pop-up.
+     *
+     *
+     * @param credentialDataStoreFactory The {@link DataStoreFactory} that should be used
+     * @param scopes The scopes for which the flow should request authorization
+     * @return Returns the {@link GoogleAnalyticsConnectorFactory} for the given {@link DataStoreFactory} and scopes
+     * @throws IOException If there is a problem accessing the {@link DataStoreFactory}
+     */
+    private static GoogleAuthorizationCodeFlow getAuthorizationCodeFlow(
+        final DataStoreFactory credentialDataStoreFactory, final List<KnimeGoogleAuthScope> knimeScopes)
+        throws IOException {
+        try (final InputStream in = GoogleAuthentication.class.getResourceAsStream(CLIENT_SECRET)) {
+            KnimeGoogleAuthScopeRegistry.getInstance();
+            final List<String> scopes = KnimeGoogleAuthScopeRegistry.getAuthScopes(knimeScopes);
+            final GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+            return new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, scopes)
+                .setDataStoreFactory(credentialDataStoreFactory).setAccessType("offline").build();
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+
+    /**
+     * Removes the credentials from the given {@link GoogleAuthLocationType}.
+     *
+     * @param type The {@link GoogleAuthLocationType} for which the credentials should be removed
+     * @param credentialPath The path to the credentials that should be removed
+     * @throws IOException If the credential location cannot be accessed
+     */
+    public static void removeCredential(final GoogleAuthLocationType type, final String credentialPath)
+        throws IOException {
+        DataStoreFactory credentialDataStoreFactory = getCredentialDataStoreFactory(type, credentialPath);
+        credentialDataStoreFactory.getDataStore(StoredCredential.DEFAULT_DATA_STORE_ID).delete(DEFAULT_KNIME_USER);
+    }
+
+    /**
+     * Decodes a given byte String to a given folder.
+     *
+     * @param tempFolder The temp folder that should be populated with the decoded byte string
+     * @param credentialByteString The byte string that should be decoded to the given folder
+     * @throws IOException If there is an error when writing the temp file
+     */
+    public static void createTempFromByteFile(final File tempFolder, final String credentialByteString)
+        throws IOException {
+        byte[] decodeBase64 = Base64.getDecoder().decode(credentialByteString);
+        Files.write(new File(tempFolder, StoredCredential.DEFAULT_DATA_STORE_ID).toPath(), decodeBase64);
+    }
+
+    /**
+     * Create a temporary file from a given byte string and returns it's location.
+     *
+     * @param credentialByteString The byte string that should be written to a temporary folder.
+     * @return The credential location
+     * @throws IOException If temporary folder cannot be created
+     */
+    public static String getTempCredentialPath(final String credentialByteString) throws IOException {
+        File tempFolder = FileUtil.createTempDir("sheets");
+        createTempFromByteFile(tempFolder, credentialByteString);
+        return tempFolder.getPath();
+    }
+
+    /**
+     * Returns the encoded storage credential from the given path
+     *
+     * @param tempfolder The path of the Credentials
+     * @return The base64 encoded file storage credential file from the given folder
+     * @throws IOException If the path cannot be read
+     */
+    public static String getByteStringFromFile(final String tempfolder) throws IOException {
+        return Base64.getEncoder()
+            .encodeToString(Files.readAllBytes(new File(tempfolder, StoredCredential.DEFAULT_DATA_STORE_ID).toPath()));
+    }
+
+}
